83b764e105786ef7064b7b75e41af7a823e17bf1
hajowieland <mail@wieland.tech>
2019-06-20 17:24:49 +0000

    Add oci public_key_path

--- provider.tf
+++ provider.tf
@@ -62,6 +62,7 @@ provider "oci" {
   user_ocid        = "${var.oci_user_ocid}"
   fingerprint      = "${var.oci_fingerprint}"
   private_key_path = "${var.oci_private_key_path}"
+  public_key_path = "${var.oci_public_key_path}"
   region           = "${var.oci_region}"
 }
 
